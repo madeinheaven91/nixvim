e892d30e7f9a154314f9e637e92da5a8b7f644fc
madeinheaven91 <exspectanss@gmail.com>
2026-02-26 19:28:16 +0300

    hurl

---
+++ config/plugins/hurl.nix
@@ -0,0 +1,24 @@
+{
+  plugins.hurl = {
+    enable = true;
+    settings = {
+      formatters = {
+        html = [
+          "prettier"
+          "--parser"
+          "html"
+        ];
+        json = [
+          "jq"
+        ];
+        xml = [
+          "tidy"
+          "-xml"
+          "-i"
+          "-q"
+        ];
+      };
+      mode = "split";
+    };
+  };
+}
